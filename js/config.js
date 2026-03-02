a83f269fff024e83bf516882c3ee797e267572a2
darren241083 <darren@koadesign.co.uk>
2026-03-02 00:01:01 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,15 @@
+export const SUPABASE_URL = "https://hkorcwsbuwreaupdbkaf.supabase.co";
+export const SUPABASE_KEY = "sb_publishable_3Qz-fLYy9x5dEPvFVu2dlw_WBlczkBW";
 
+export const STORAGE = {
+  role: "ps_role_v1",
+  pupil: "ps_pupil_v1",
+  teacherLastTest: "ps_teacher_last_test_v1"
+};
+
+export const DEFAULT_GRAPHEME_BANK = [
+  "a","e","i","o","u",
+  "ai","ay","a-e","ee","ea","ie","igh","oa","oo","or","ar","ur","er",
+  "ch","sh","th","ng","ph","wh",
+  "b","c","d","f","g","h","j","k","l","m","n","p","qu","r","s","t","v","w","y","z"
+];
