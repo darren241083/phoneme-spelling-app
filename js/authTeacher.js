6be94b818064fcc935dd65c8dd43a3bc2c7c9731
darren241083 <darren@koadesign.co.uk>
2026-03-02 00:01:56 +0000

    Update authTeacher.js

--- js/authTeacher.js
+++ js/authTeacher.js
@@ -1,1 +1,20 @@
+import { supabase } from "./supabaseClient.js";
 
+export async function teacherSignInGoogle(){
+  const redirectTo = window.location.origin + window.location.pathname;
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: { redirectTo }
+  });
+  if (error) throw error;
+}
+
+export async function signOut(){
+  await supabase.auth.signOut();
+}
+
+export async function getSession(){
+  const { data, error } = await supabase.auth.getSession();
+  if (error) throw error;
+  return data.session;
+}
